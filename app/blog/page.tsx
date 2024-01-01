46dc3b5fdbff8d7c348b007de969eea8e4a5fb69
Abisma <imabubakar508@gmail.com>
2024-01-01 10:47:59 +0300

    reafactor: grid-spacing

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -27,7 +27,7 @@ const page = async () => {
     <div className=' lg:px-24 px-2 mb-10'>
       <h1 className=' text-xl font-bold'>Latest Blog</h1>
     </div>
-    <div className=' lg:px-24 px-2 grid md:grid-cols-2 lg:grid-cols-4'>
+    <div className=' lg:px-24 px-2 grid gap-3 lg:gap-4 md:grid-cols-2 lg:grid-cols-4'>
         {data.map((post: any, index: number) => (
           <BlogCard post={post} index={index} />
         ))}
